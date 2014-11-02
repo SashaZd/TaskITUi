ad4a59404733361f378e5c1e3524343bfb301984
Rahul Goel <irodov.rg@gmail.com>
2014-11-02 01:21:42 -0400

    changed bordercase and grocery add width

--- TaskIt/app/view/Groceries.js
+++ TaskIt/app/view/Groceries.js
@@ -7,6 +7,7 @@ myNewGroceryItem = new Ext.Panel({
                 xtype: 'textfield',
                 name: 'name',
                 id : 'addNewGroceryItem',
+                label : 'Item Name',
                 width : '80%'
 
             },
@@ -14,7 +15,7 @@ myNewGroceryItem = new Ext.Panel({
             {
                 xtype : 'button',
                 text : 'Add',
-                width : '18%',
+                width : '24%',
                 handler : function(){
                     // Ext.Ajax.request({
                     //     url: 'page.php',
